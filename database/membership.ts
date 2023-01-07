e3e1dd05e6e79bee2500cb7b47d95f9a4da03df9
Jackson Quintero <codigoarchivo@gmail.com>
2023-01-06 21:06:12 -0400

    database/: creo la database estatica secciones

---
+++ database/membership.ts
@@ -0,0 +1,50 @@
+import { IMembership } from "../interfaces";
+
+
+export const membership: IMembership[] = [
+    {
+        title: 'Investigación energética',
+        seccion1: '1 seción 100$',
+        seccion2: '2 secciones 150$',
+    },
+    {
+        title: 'Adicciones',
+        seccion1: '1 seción 150$',
+        seccion2: '2 secciones 250$',
+        seccion3: '3 secciones 300$',
+    },
+    {
+        title: 'Armonización de chakras',
+        seccion1: '1 seción 100$',
+        seccion2: '2 secciones 150$',
+        seccion3: '3 secciones 300$',
+    },
+    {
+        title: 'Pareja',
+        seccion1: '1 seción 80$',
+        seccion2: '2 secciones 120$',
+    },
+    {
+        title: 'Sanación',
+        seccion1: '1 seción 150$',
+        seccion2: '2 secciones 200$',
+        seccion3: '3 secciones 250$',
+    },
+    {
+        title: 'Transformación',
+        seccion1: '1 seción 150$',
+        seccion2: '2 secciones 200$',
+        seccion3: '3 secciones 250$',
+    },
+    {
+        title: 'Árbol genealógico',
+        seccion1: '1 seción 150$',
+        seccion2: '2 secciones 200$',
+        seccion3: '3 secciones 250$',
+    },
+    {
+        title: 'Reprogramación energética de tu marca',
+        seccion1: '1 seción 300$',
+        seccion2: '2 secciones 500$',
+    },
+]
