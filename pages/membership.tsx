417029c571054a2458d25d13e2561f4864854c3b
Jackson Quintero <codigoarchivo@gmail.com>
2023-01-07 18:04:42 -0400

    pages/membership.tsx: quite varias clases

--- pages/membership.tsx
+++ pages/membership.tsx
@@ -42,67 +42,63 @@ const membership: NextPage = () => {
             </div>
 
             {!toggle ? (
-              <div className="pricing-base">
+              <div className="pricing-body-plans">
                 {member.membership.map((item) => (
-                  <div key={item.title} className="pricing-body-plans">
-                    <div className="card box-shadow">
-                      <div className="card-header">
-                        <h1 className="card-title">{item.title}</h1>
-                      </div>
-                      <div className="card-body">
-                        <ul>
-                          <li>
-                            <span>{item.seccion1.slice(0, -4)}</span>
-                            <span>{item.seccion1.slice(8, 13)}</span>
-                          </li>
-                          <li>
-                            <span>{item.seccion2.slice(0, -4)}</span>
-                            <span>{item.seccion2.slice(11, 16)}</span>
-                          </li>
+                  <div key={item.title} className="card box-shadow">
+                    <div className="card-header">
+                      <h1 className="card-title">{item.title}</h1>
+                    </div>
+                    <div className="card-body">
+                      <ul>
+                        <li>
+                          <span>{item.seccion1.slice(0, -4)}</span>
+                          <span>{item.seccion1.slice(8, 13)}</span>
+                        </li>
+                        <li>
+                          <span>{item.seccion2.slice(0, -4)}</span>
+                          <span>{item.seccion2.slice(11, 16)}</span>
+                        </li>
 
-                          {item?.seccion3 && (
-                            <li>
-                              <span>{item?.seccion3.slice(0, -4)}</span>
-                              <span>{item?.seccion3.slice(11, 16)}</span>
-                            </li>
-                          )}
-                        </ul>
-                      </div>
-                      <div className="card-footer">
-                        <button
-                          className="box-shadow"
-                          onClick={() => handleSelected(item)}
-                        >
-                          Choose Plan
-                        </button>
-                      </div>
+                        {item?.seccion3 && (
+                          <li>
+                            <span>{item?.seccion3.slice(0, -4)}</span>
+                            <span>{item?.seccion3.slice(11, 16)}</span>
+                          </li>
+                        )}
+                      </ul>
+                    </div>
+                    <div className="card-footer">
+                      <button
+                        className="box-shadow"
+                        onClick={() => handleSelected(item)}
+                      >
+                        Choose Plan
+                      </button>
                     </div>
                   </div>
                 ))}
               </div>
             ) : (
-              <div className="pricing-base">
-                <div className="pricing-body-plans">
-                  <div className="card box-shadow">
-                    <div className="card-header">
-                      <span className="card-title">
-                        E(Empresarial) afiliación de tienda virtual
-                      </span>
-                      <h2 className="card-price">50$ 1/año</h2>
-                    </div>
-                    <div className="card-body">
-                      <ul>
-                        <li>500 MAUs</li>
-                        <li>3 projects</li>
-                        <li>Unlimited guides</li>
-                        <li>Unlimited flows</li>
-                        <li>Unlimited branded themes</li>
-                        <li>Email support</li>
-                      </ul>
-                    </div>
-                    <div className="card-footer">
-                      <button className="box-shadow">Choose Plan</button>
-                    </div>
+              <div className="pricing-body-plans">
+                <div className="card box-shadow">
+                  <div className="card-header">
+                    <span className="card-title">
+                      E(Empresarial) afiliación de tienda virtual
+                    </span>
+                    <h2 className="card-price">50$ 1/año</h2>
+                  </div>
+                  <div className="card-body">
+                    <ul>
+                      <li>500 MAUs</li>
+                      <li>3 projects</li>
+                      <li>Unlimited guides</li>
+                      <li>Unlimited flows</li>
+                      <li>Unlimited branded themes</li>
+                      <li>Email support</li>
+                    </ul>
+                  </div>
+                  <div className="card-footer">
+                    <button className="box-shadow">Choose Plan</button>
                   </div>
                 </div>
               </div>
