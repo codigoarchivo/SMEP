e5399d6b6b6c6f59b70acc38644a373968711612
Jackson Quintero <codigoarchivo@gmail.com>
2023-01-10 16:57:15 -0400

    context/membership/membershipReducer.ts: crearon los estados para sesion y business

--- context/membership/membershipReducer.ts
+++ context/membership/membershipReducer.ts
@@ -1,13 +1,21 @@
 import { MembershipState } from './';
+import { IBusiness, ISesion } from '../../interfaces';
 
 type MembershipActionType =
-    | { type: '[Membership] - List' }
+    | { type: '[Sesion] - Select', payload: ISesion }
+    | { type: '[Business] - Select', payload: IBusiness }
 
 export const MembershipReducer = (state: MembershipState, action: MembershipActionType): MembershipState => {
     switch (action.type) {
-        case '[Membership] - List':
+        case '[Sesion] - Select':
             return {
                 ...state,
+                sesion: action.payload
+            }
+        case '[Business] - Select':
+            return {
+                ...state,
+                business: action.payload
             }
         default:
             return state;
