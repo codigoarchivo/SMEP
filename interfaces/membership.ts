3847def6e49158910f754413f5ddada3301b8d44
Jackson Quintero <codigoarchivo@gmail.com>
2023-01-10 17:00:09 -0400

    interfaces/membership.ts: creo las interfaces para sesion y business

--- interfaces/membership.ts
+++ interfaces/membership.ts
@@ -1,6 +1,30 @@
 export interface IMembership {
     title: string,
-    seccion1: string,
-    seccion2: string,
-    seccion3?: string,
+    price?: string,
+    desc1: string,
+    desc2: string,
+    desc3?: string,
+}
+
+export interface ISesion {
+    title: string,
+    sesion1?: {
+        description: string,
+        nSesion: number,
+        price: number,
+    },
+    sesion2?: {
+        description: string,
+        nSesion: number,
+        price: number,
+    }
+}
+
+export interface IBusiness {
+    title: string
+    price: number,
+    desc1: string,
+    desc2: string,
+    age: number,
+    priceMonth: number
 }
