b4e7ee41b8373f6123ea4f33c9be381db353c205
Jackson Quintero <codigoarchivo@gmail.com>
2023-01-13 17:28:51 -0400

    database/membership.ts: cambio de planes

--- database/membership.ts
+++ database/membership.ts
@@ -3,81 +3,37 @@ import { IMembership } from "../interfaces";
 
 export const membership: IMembership[] = [
     {
-        title: 'Investigación energética',
-        desc1: '1 sesión 100$',
-        desc2: '2 sesiones 150$',
+        title: 'ADICCIONES',
+        price: '150$ x sesión',
     },
     {
-        title: 'Adicciones',
-        desc1: '1 sesión 150$',
-        desc2: '2 sesiones 250$',
-        desc3: '3 sesiones 300$',
+        title: 'ARMONIZACIÓN DE CHAKRAS',
+        price: '100$ x sesión',
     },
     {
-        title: 'Armonización de chakras',
-        desc1: '1 sesión 100$',
-        desc2: '2 sesiones 150$',
-        desc3: '3 sesiones 300$',
+        title: 'PAREJA',
+        price: '100$ x sesión',
     },
     {
-        title: 'Pareja',
-        desc1: '1 sesión 80$',
-        desc2: '2 sesiones 120$',
+        title: 'SANACIÓN',
+        price: '100$ x sesión'
     },
     {
-        title: 'Sanación',
-        desc1: '1 sesión 150$',
-        desc2: '2 sesiones 200$',
-        desc3: '3 sesiones 250$',
+        title: 'SALUD',
+        price: '150$ x sesión'
     },
     {
-        title: 'Transformación',
-        desc1: '1 sesión 150$',
-        desc2: '2 sesiones 200$',
-        desc3: '3 sesiones 250$',
-    },
-    {
-        title: 'Árbol genealógico',
-        desc1: '1 sesión 150$',
-        desc2: '2 sesiones 200$',
-        desc3: '3 sesiones 250$',
-    },
-    {
-        title: 'Reprogramación energética de tu marca',
-        desc1: '1 sesión 300$',
-        desc2: '2 sesiones 500$',
-    },
-    {
-        title: 'Enfoque deportivo',
-        desc1: '1 sesión 150$',
-        desc2: '2 sesiones 200$',
-        desc3: '3 sesiones 250$',
+        title: 'ENFOQUE DEPORTIVO',
+        price: '150$ x sesión',
     },
 ]
 
 export const membershiAge: IMembership[] = [
     {
-        title: 'Empresarial; Plus.',
-        price: '11 $/mes × 1 año',
+        title: 'PLAN EMPRESARIAL',
+        price: '25$/mes x 1 año',
         desc1: 'Suscripción',
-        desc2: 'publicidad',
-        desc3: 'link',
-        desc4: '1 reprogramación energética de tu marca',
-    },
-    {
-        title: 'Empresarial; Profesional.',
-        price: '15$/mes 2 años',
-        desc1: 'Suscripción',
-        desc2: 'publicidad',
-        desc3: 'link',
-        desc4: '2 reprogramación energética de tu marca',
-    },
-    {
-        title: 'Empresarial; Prémium.',
-        price: '20$/mes 3 años',
-        desc1: 'Suscripción',
-        desc2: 'publicidad',
-        desc3: 'link',
-        desc4: '3 reprogramación energética de tu marca',
+        desc2: 'Publicidad',
+        desc3: '2 reprogramación energética de tu marca',
     },
 ]
