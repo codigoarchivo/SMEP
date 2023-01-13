07672f294e648cee3e878719ea3f67e9d9dc3ba0
Jackson Quintero <codigoarchivo@gmail.com>
2023-01-13 17:34:48 -0400

    pages/membership.tsx: agrego la funcionalidad selectedCheck

--- pages/membership.tsx
+++ pages/membership.tsx
@@ -1,15 +1,15 @@
 import { useState, useContext } from 'react';
 import { NextPage } from 'next';
+import { useRouter } from 'next/router';
 import { Paper, Button, Container } from '@mui/material';
 import { member } from '../database';
 import { IMembership } from '../interfaces';
 import { calcAge } from '../helpers';
 import { MembershipContext } from '../context/membership';
-import { useRouter } from 'next/router';
 
 const membership: NextPage = () => {
   const [toggle, setToggle] = useState(false);
-  const { selectedSesion, selectedBusiness } = useContext(MembershipContext);
+  const { selectedCheck } = useContext(MembershipContext);
   const { push } = useRouter();
 
   const handleCheck = () => {
@@ -17,28 +17,20 @@ const membership: NextPage = () => {
   };
 
   const handleSelected = (data: IMembership) => {
-    toggle
-      ? selectedBusiness({
-          ...data,
-          age: Number(data.price!.slice(6, 8).trim()),
-          priceMonth: Number(data.price!.slice(0, 1).trim()),
-          price:
-            Number(data.price!.slice(0, 1).trim()) *
-            calcAge(Number(data.price!.slice(6, 8).trim())),
-        })
-      : selectedSesion({
-          title: data.title,
-          sesion1: {
-            description: data.desc1,
-            price: Number(data.desc1.slice(8, 12).trim()),
-            nSesion: Number(data.desc1.slice(0, 1).trim()),
-          },
-          sesion2: {
-            description: data.desc2,
-            price: Number(data.desc2.slice(11, 14).trim()),
-            nSesion: Number(data.desc2.slice(0, 1).trim()),
-          },
-        });
+    selectedCheck(
+      toggle
+        ? {
+            ...data,
+            monthT: calcAge(Number(data.price!.slice(10, 11).trim())),
+            priceU: Number(data.price!.slice(0, 2).trim()),
+            repro: Number(data.price!.slice(0, 1).trim()),
+          }
+        : {
+            title: data.title,
+            priceS: Number(data.price?.slice(0, 3).trim()),
+          }
+    );
+
     push('/selectMembership');
   };
 
@@ -49,7 +41,10 @@ const membership: NextPage = () => {
       <section className='pricing-section'>
         <div className='pricing'>
           <div className='pricing-header'>
-            <h3>E D G A R ' S P E N D U L U M</h3>
+            <h3>
+              <span>E D G A R ' S</span>
+              <span>P E N D U L U M</span>
+            </h3>
           </div>
           <div className='pricing-body'>
             <div className='pricing-body-header'>
@@ -77,31 +72,25 @@ const membership: NextPage = () => {
                   <Paper key={item.title} className='card' elevation={2}>
                     <div className='card-header'>
                       <span className='card-title'>{item.title}</span>
-                      <h2 className='card-price'>{item.price}</h2>
+                      {toggle && <h2 className='card-price'>{item.price}</h2>}
                     </div>
                     <div className='card-body'>
                       <ul>
                         {toggle ? (
                           <>
-                            <li>{item.desc1}</li>
-                            <li>{item.desc2}</li>
+                            <li>
+                              <span>{item.desc1}</span>
+                            </li>
+                            <li>
+                              <span>{item.desc2}</span>
+                            </li>
+                            <li>
+                              <span>{item.desc3}</span>
+                            </li>
                           </>
                         ) : (
-                          <>
-                            <li>
-                              <span>{item.desc1.slice(0, -4)}</span>
-                              <span>{item.desc1.slice(8, 13)}</span>
-                            </li>
-                            <li>
-                              <span>{item.desc2.slice(0, -4)}</span>
-                              <span>{item.desc2.slice(11, 16)}</span>
-                            </li>
-                          </>
-                        )}
-                        {item?.desc3 && (
                           <li>
-                            <span>{item?.desc3.slice(0, -4)}</span>
-                            <span>{item?.desc3.slice(11, 16)}</span>
+                            <span>{item.price}</span>
                           </li>
                         )}
                       </ul>
