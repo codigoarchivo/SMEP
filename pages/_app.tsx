9081b4b9c44331ccdd6ac86b95865254b5ae1541
Jackson Quintero <codigoarchivo@gmail.com>
2023-01-26 19:01:07 -0400

    pages/_app.tsx: agregue sesionProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,18 @@
+import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
-import { ColorsProvider, MembershipProvider } from "../context";
+import { AuthProvider, ColorsProvider, MembershipProvider } from "../context";
 import "../scss/styles.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <MembershipProvider>
-      <ColorsProvider>
-        <Component {...pageProps} />
-      </ColorsProvider>
-    </MembershipProvider>
+    <SessionProvider>
+      <AuthProvider>
+        <MembershipProvider>
+          <ColorsProvider>
+            <Component {...pageProps} />
+          </ColorsProvider>
+        </MembershipProvider>
+      </AuthProvider>
+    </SessionProvider>
   );
 }
