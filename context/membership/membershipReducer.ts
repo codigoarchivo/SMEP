be37dc403b35c52be92718060348745864afb8fa
Jackson Quintero <codigoarchivo@gmail.com>
2023-01-30 18:54:04 -0400

    context/membership/membershipReducer.ts creo buy

--- context/membership/membershipReducer.ts
+++ context/membership/membershipReducer.ts
@@ -1,15 +1,21 @@
 import { MembershipState } from './';
-import { ICheck } from '../../interfaces';
+import { ICheck, ISessionOrSubscription } from '../../interfaces';
 
 type MembershipActionType =
     | { type: '[Check] - all', payload: ICheck }
+    | { type: '[buy] - Session or Subscription', payload: ISessionOrSubscription }
 
-export const MembershipReducer = (state: MembershipState, action: MembershipActionType): MembershipState => {
+export const membershipReducer = (state: MembershipState, action: MembershipActionType): MembershipState => {
     switch (action.type) {
         case '[Check] - all':
             return {
                 ...state,
                 check: action.payload
+            }
+        case '[buy] - Session or Subscription':
+            return {
+                ...state,
+                buy: [...state.buy, action.payload]
             }
         default:
             return state;
