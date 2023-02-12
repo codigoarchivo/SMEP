5225fdba94b493d0e3a77de67726b4832fd9615a
Jackson Quintero <codigoarchivo@gmail.com>
2023-02-12 16:56:20 -0400

    context/membership/MembershipProvider.tsx: agrego buy provider

--- context/membership/MembershipProvider.tsx
+++ context/membership/MembershipProvider.tsx
@@ -1,15 +1,15 @@
-import { FC, useReducer, ReactNode, useEffect } from "react";
-import Cookies from "js-cookie";
-import { ICheck, ISessionOrSubscription, IUserLem } from "../../interfaces";
-import { MembershipContext, membershipReducer } from "./";
-import { isEmpty } from "../../helpers";
-import { mbepApi } from "../../api";
-import { AuthProvider } from "../auth/AuthProvider";
-import { useSession } from "next-auth/react";
+import { FC, useReducer, ReactNode, useEffect } from 'react';
+import Cookies from 'js-cookie';
+import { ICheck, ISubscription, IUserLem } from '../../interfaces';
+import { MembershipContext, membershipReducer } from './';
+import { isEmpty } from '../../helpers';
+import { mbepApi } from '../../api';
+import { AuthProvider } from '../auth/AuthProvider';
+import { useSession } from 'next-auth/react';
 
 export interface MembershipState {
   check: ICheck;
-  buy: ISessionOrSubscription[];
+  sub: ISubscription[];
 }
 
 interface Props {
@@ -18,20 +18,37 @@ interface Props {
 
 const MEMBERSHIP_INITIAL_STATE: MembershipState = {
   check: {},
-  buy: [],
+  sub: [],
 };
 
 export const MembershipProvider: FC<Props> = ({ children }) => {
   const el = useSession();
+  /* Getting the user from the session. */
+  const user = { ...el.data?.user } as IUserLem;
 
   useEffect(() => {
-    const cookieCheck = Cookies.get("check")
-      ? JSON.parse(Cookies.get("check")!)
+    const cookieCheck = Cookies.get('check')
+      ? JSON.parse(Cookies.get('check')!)
       : {};
     try {
-      dispatch({ type: "[Check] - all", payload: cookieCheck });
+      dispatch({ type: '[Check] - all', payload: cookieCheck });
     } catch (error) {
-      dispatch({ type: "[Check] - all", payload: {} as ICheck });
+      dispatch({ type: '[Check] - all', payload: {} as ICheck });
+    }
+  }, []);
+
+  useEffect(() => {
+    const sub = Cookies.get('sub') ? JSON.parse(Cookies.get('sub')!) : [];
+    try {
+      dispatch({
+        type: '[Sub] - Subscription',
+        payload: sub,
+      });
+    } catch (error) {
+      dispatch({
+        type: '[Sub] - Subscription',
+        payload: [] as ISubscription[],
+      });
     }
   }, []);
 
@@ -40,34 +57,31 @@ export const MembershipProvider: FC<Props> = ({ children }) => {
     MEMBERSHIP_INITIAL_STATE
   );
 
-  useEffect(() => {
-    if (!isEmpty(state.check)!)
-      Cookies.set("check", JSON.stringify(state.check));
-  }, [state.check]);
-
-  const selectedCheck = (check: ICheck) => {
-    dispatch({ type: "[Check] - all", payload: check });
+  const selectSave = (name: string, data: ICheck | ISubscription[]) => {
+    Cookies.set(`${name}`, JSON.stringify(data));
   };
 
-  const sessionOrSubscription = async (buy: ISessionOrSubscription) => {
-    /* Getting the user from the session. */
-    // const { role, ...user } = { ...el.data?.user } as IUserLem;
+  const selectedCheck = (check: ICheck) => {
+    dispatch({ type: '[Check] - all', payload: check });
+    selectSave('check', check);
+  };
 
-    // console.log(user);
+  const subscription = async (dataSub: ISubscription) => {
+    const addBuy = { ...dataSub, user: user._id, valid: false };
 
-    const { data, statusText } = await mbepApi.post(`/membership/subcription`, buy);
-    console.log({ data, statusText });
-
-    // if (statusText === "OK") {
-    //   await mbepApi.post("/admin/membership", {
-    //     name: user.name,
-    //     email: user.email,
-    //     _id: data._id,
-    //     valid: false,
-    //   });
-    // }
-
-    dispatch({ type: "[Check] - all", payload: buy });
+    const { data, statusText } = await mbepApi.post(
+      `/membership/subscription`,
+      addBuy
+    );
+   
+    if (statusText === 'OK') {
+      await mbepApi.post('/admin/order', {
+        buy: data._id,
+      });
+      const sub = [addBuy, ...state.sub];
+      dispatch({ type: '[Sub] - Subscription', payload: sub });
+      selectSave('sub', sub);
+    }
   };
 
   return (
@@ -77,7 +91,7 @@ export const MembershipProvider: FC<Props> = ({ children }) => {
 
         //method
         selectedCheck,
-        sessionOrSubscription,
+        subscription,
       }}
     >
       {children}
