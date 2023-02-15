a4908e76c4bd1fb3ab74061f183882656a891a0f
Jackson Quintero <codigoarchivo@gmail.com>
2023-02-15 16:25:09 -0400

    pages/api/admin/order.ts: api order

---
+++ pages/api/admin/order.ts
@@ -0,0 +1,41 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { Session } from 'next-auth';
+import { getSession } from 'next-auth/react';
+import { db } from '../../../database';
+import { IOrder } from '../../../interfaces';
+import { Order } from '../../../models';
+
+type membershipData =
+  | { message: string }
+  | IOrder  
+
+export default function handle(req: NextApiRequest, res: NextApiResponse<membershipData>) {
+
+  switch (req.method) {
+    case 'POST':
+      return createMembership(req, res)
+    default:
+      return res.status(400).json({ message: 'Bad request' })
+  }
+
+
+}
+
+const createMembership = async (req: NextApiRequest, res: NextApiResponse<membershipData>) => {
+
+  const subscription: Session | null = await getSession({ req });
+  if (!subscription) res.status(401).json({ message: 'You must be authenticated to do this' })
+
+  try {
+    await db.connect();
+    const order = new Order({ ...req.body });
+    await order.save();
+    await db.disconnect();
+
+    return res.status(201).json(order)
+
+  } catch (error) {
+    await db.disconnect();
+    res.status(400).json({ message: 'Bad request' })
+  }
+}
