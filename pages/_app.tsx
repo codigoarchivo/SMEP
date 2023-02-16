74039f79890bbc76dc6464b0209125ac1d0ffbe3
Jackson Quintero <codigoarchivo@gmail.com>
2023-02-16 18:24:19 -0400

    pages/_app.tsx: SWRConfig

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,26 @@
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import { SWRConfig } from "swr/_internal";
 import { AuthProvider, ColorsProvider, MembershipProvider } from "../context";
 import "../scss/styles.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider>
-      <AuthProvider>
-        <MembershipProvider>
-          <ColorsProvider>
-            <Component {...pageProps} />
-          </ColorsProvider>
-        </MembershipProvider>
-      </AuthProvider>
+      <SWRConfig
+        value={{
+          fetcher: (resource, init) =>
+            fetch(resource, init).then((res) => res.json()),
+        }}
+      >
+        <AuthProvider>
+          <MembershipProvider>
+            <ColorsProvider>
+              <Component {...pageProps} />
+            </ColorsProvider>
+          </MembershipProvider>
+        </AuthProvider>
+      </SWRConfig>
     </SessionProvider>
   );
 }
