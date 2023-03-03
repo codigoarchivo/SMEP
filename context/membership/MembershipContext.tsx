6fc909cd4ad7a68f85b420630b78abf1bbc8f07b
Jackson Quintero <codigoarchivo@gmail.com>
2023-03-03 14:55:31 -0400

    context/membership/MembershipContext.tsx: agregue dos select

--- context/membership/MembershipContext.tsx
+++ context/membership/MembershipContext.tsx
@@ -1,19 +1,26 @@
 import { createContext } from 'react';
-import { ICheck, ISession, ISubscription } from '../../interfaces';
+import {
+  ISelectSession,
+  ISelectSubscription,
+  ISession,
+  ISubscription,
+} from '../../interfaces';
 
 interface ContextProps {
-  check: ICheck;
+  check: ISelectSubscription | ISelectSession;
   sub: ISubscription[];
   ses: ISession[];
 
   // method
+  selectedSession: (check: ISelectSession) => void;
+  selectedSubscription: (check: ISelectSubscription) => void;
   listSession: (data: ISession[]) => void;
   listSubscription: (data: ISubscription[]) => void;
-  selectedCheck: (business: ICheck) => void;
   subscription: (data: ISubscription) => void;
   session: (data: ISession) => void;
   deleteOne: (id: string) => void;
   deleteTwo: (id: string) => void;
+  addRenewal: (dataSub: ISubscription) => Promise<void>;
 }
 
 export const MembershipContext = createContext({} as ContextProps);
