9d24755a3897d336d516d6e34820abb782a9626c
Jackson Quintero <codigoarchivo@gmail.com>
2023-03-11 21:03:42 -0400

    helpers/calcAge.ts: calcAge calcDays limite

--- helpers/calcAge.ts
+++ helpers/calcAge.ts
@@ -1,19 +1,23 @@
+export const calcAge = () => {
+    let d = new Date();
+    const c = new Date(d.getFullYear() + 1, d.getMonth(), d.getDate());
+    return c
+}
 
-export const calcAge = (data: number) => {
-    let age = 0;
-    switch (data) {
-        case 1:
-            age = 12;
-            break;
-        case 2:
-            age = 24;
-            break;
-        case 3:
-            age = 36;
-            break;
-        default:
-            age = 0;
-            break;
-    }
-    return age
+export const calcDays = (d: Date) => {
+    let days = [];
+    let i = new Date();
+    for (i; i <= d; i.setDate(i.getDate() + 1)) days.push(new Date(i));
+    if (days.length <= 0) return;
+    return days.length;
+}
+
+export const limite = (d: number | Date) => {
+    let words = ''
+    if (d === undefined) return words = 'se agoto el tiempo';
+    if (d >= 15 && d <= 20) return words = '20 dias para la renovación';
+    if (d >= 10 && d <= 15) return words = '15 dias para la renovación';
+    if (d >= 5 && d <= 10) return words = '10 dias para la renovación';
+    if (d >= 1 && d <= 5) return words = `${d} dias para la renovación`;
+    if (d) return words = `${calcAge().toLocaleDateString()}`;
 }
