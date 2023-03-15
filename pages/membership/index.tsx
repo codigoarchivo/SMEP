a02f3fd07a9b52603022a3d1b0d0eaf32f71490e
Jackson Quintero <codigoarchivo@gmail.com>
2023-03-14 20:52:47 -0400

    pages/membership/index.tsx: se cambio estructura

--- pages/membership/index.tsx
+++ pages/membership/index.tsx
@@ -1,15 +1,17 @@
-import { useState, useContext } from "react";
-import { NextPage } from "next";
-import { useRouter } from "next/router";
-import { Paper, Button, Container } from "@mui/material";
-import { member } from "../../database";
-import { IMembership } from "../../interfaces";
-import { calcAge, returnNumber } from "../../helpers";
-import { MembershipContext } from "../../context/membership";
+import { useState, useContext } from 'react';
+import { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { Paper, Button, Container } from '@mui/material';
+import { IMembership } from '../../interfaces';
+import { returnNumber } from '../../helpers';
+import { MembershipContext } from '../../context/membership';
+import { membershiAge, membership } from '../../static';
 
-const membership: NextPage = () => {
+const Membership: NextPage = () => {
   const [toggle, setToggle] = useState(false);
-  const { selectedCheck } = useContext(MembershipContext);
+  const { selectedSession, selectedSubscription } = useContext(
+    MembershipContext
+  );
   const { push } = useRouter();
 
   const handleCheck = () => {
@@ -17,64 +19,62 @@ const membership: NextPage = () => {
   };
 
   const handleSelected = (data: IMembership) => {
-    selectedCheck(
-      toggle
-        ? {
-            ...data,
-            monthT: calcAge(1),
-            priceU: returnNumber(data.price?.slice(0, 8)),
-            repro: returnNumber(data.desc3),
-          }
-        : {
-            title: data.title,
-            priceU: returnNumber(data.price),
-          }
-    );
+    toggle
+      ? selectedSubscription({
+          ...data,
+          monthT: 12,
+          priceU: returnNumber(data.price?.slice(0, 8)),
+          repro: returnNumber(data.desc3),
+        })
+      : selectedSession({
+          title: data.title,
+          priceU: returnNumber(data.price),
+        });
 
-    push("/membership/select");
+    push('/membership/select');
   };
 
-  const data: IMembership[] = toggle ? member.membershiAge : member.membership;
+  const data = toggle ? membershiAge.plans : membership.plans;
 
   return (
     <Container>
-      <section className="pricing-section">
-        <div className="pricing">
-          <div className="pricing-header">
+      <section className='pricing-section'>
+        <div className='pricing'>
+          <div className='pricing-header'>
             <h3>
-              <span>E D G A R ' S</span>
+              <span>{"E D G A R ' S"}</span>
               <span>P E N D U L U M</span>
             </h3>
           </div>
-          <div className="pricing-body">
-            <div className="pricing-body-header">
+          <div className='pricing-body'>
+            <div className='pricing-body-header'>
               <h2>Elige un plan</h2>
-              <div className="pricing-checkbox">
-                <span className={!toggle ? "text-light" : ""}>
+              <div className='pricing-checkbox'>
+                <span className={!toggle ? 'text-light' : ''}>
                   Facturado por sesiones
                 </span>
                 <div
-                  className={toggle ? "anually" : ""}
+                  className={toggle ? 'anually' : ''}
                   onClick={handleCheck}
-                  id="custom-checkbox"
+                  id='custom-checkbox'
                 >
-                  <div className="annual"></div>
+                  <div className='annual'></div>
                 </div>
-                <span className={toggle ? "text-light" : ""}>
+                <span className={toggle ? 'text-light' : ''}>
                   Facturado anualmente
                 </span>
               </div>
             </div>
 
-            <div className="pricing-body-plans">
+            <div className='pricing-body-plans'>
               <>
                 {data.map((item) => (
-                  <Paper key={item.title} className="card" elevation={2}>
-                    <div className="card-header">
-                      <span className="card-title">{item.title}</span>
-                      {toggle && <h2 className="card-price">{item.price}</h2>}
+                  <Paper key={item.title} className='card' elevation={2}>
+                    <div className='card-header'>
+                      <span className='card-title'>{item.title}</span>
+                      {toggle && <h2 className='card-price'>{item.price}</h2>}
                     </div>
-                    <div className="card-body">
+                    <div className='card-body'>
                       <ul>
                         {toggle ? (
                           <>
@@ -95,10 +95,10 @@ const membership: NextPage = () => {
                         )}
                       </ul>
                     </div>
-                    <div className="card-footer">
+                    <div className='card-footer'>
                       <Button
                         onClick={() => handleSelected(item)}
-                        color="primary"
+                        color='primary'
                       >
                         Elija plan
                       </Button>
@@ -114,4 +114,4 @@ const membership: NextPage = () => {
   );
 };
 
-export default membership;
+export default Membership;
