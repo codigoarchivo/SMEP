b63d0a7c0b4a1e72dd10492b858f3a15a6d34538
Jackson Quintero <codigoarchivo@gmail.com>
2023-03-18 20:03:34 -0400

    pages/api/membership/session.ts: updateSessionData

--- pages/api/membership/session.ts
+++ pages/api/membership/session.ts
@@ -1,25 +1,67 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { Session } from 'next-auth';
+import { getSession } from 'next-auth/react';
+import { db } from '../../../database';
+import { ISession } from '../../../interfaces';
+import { SessionModel } from '../../../models';
 
 type membershipSelectData =
     | { message: string }
-    | { _id: string }
+    | ISession | null
+
 
 export default function handle(req: NextApiRequest, res: NextApiResponse<membershipSelectData>) {
 
     switch (req.method) {
         case 'POST':
-            return createMembershipSelect(req, res)
+            return createSession(req, res);
+        case 'PUT':
+            return updateSession(req, res);
         default:
             return res.status(400).json({ message: 'Bad request' })
     }
 
 
 }
+const createSession = async (req: NextApiRequest, res: NextApiResponse<membershipSelectData>) => {
+    const session: Session | null = await getSession({ req });
+    if (!session) return res.status(401).json({ message: 'You must be authenticated to do this' });
 
-const createMembershipSelect = (req: NextApiRequest, res: NextApiResponse<membershipSelectData>) => {
-    /* Destructuring the token from the cookies. */
-    // console.log(req.cookies);
-    
-    // const { token = '' } = req.cookies;
-    return res.status(200).json({ _id: 'createMembership' })
+    try {
+        await db.connect();
+        const newSession = new SessionModel(req.body)
+        await newSession.save();
+        await db.disconnect();
+
+        return res.status(201).json(newSession)
+    } catch (error) {
+        console.log(error);
+        await db.disconnect();
+        return res.status(400).json({ message: 'Check the server console' })
+    }
+}
+
+const updateSession = async (req: NextApiRequest, res: NextApiResponse<membershipSelectData>) => {
+    const session: Session | null = await getSession({ req });
+    if (!session) return res.status(401).json({ message: 'You must be authenticated to do this' });
+
+    await db.connect();
+    const listOneData = await SessionModel.findOne({ _id: req.body.id }).lean();
+    await db.disconnect();
+
+    if (listOneData?.valid) return res.status(404).json({ message: 'It is already being processed' })
+
+    try {
+        await db.connect();
+        const updateSessionData = await SessionModel.findOneAndUpdate({ _id: req.body.id }, { $set: { state: false } }).lean()
+        await db.disconnect();
+
+        res.status(200).json(updateSessionData)
+
+    } catch (error) {
+        console.log(error);
+        await db.disconnect();
+        return res.status(400).json({ message: 'Check the server console' })
+    }
+
 }
