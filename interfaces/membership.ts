60d82782b36e2716c2802595f29a4c02019e6aed
Jackson Quintero <codigoarchivo@gmail.com>
2023-03-20 21:09:33 -0400

    interfaces/membership.ts

--- interfaces/membership.ts
+++ interfaces/membership.ts
@@ -1,33 +1,63 @@
 export interface IMembership {
     title: string,
-    price?: string,
-    desc1?: string,
-    desc2?: string,
-    desc3?: string,
-    desc4?: string,
+    price: string,
+    desc1: string,
+    desc2: string,
+    desc3: string,
+    desc4: string,
 }
 
-export interface ICheck {
-    title?: string,
-    desc1?: string,
-    desc2?: string,
-    desc3?: string,
-    repro?: number,
-    price?: string,
-    monthT?: number,
-    priceU?: number,
+export interface ISelectSession {
+    title: string,
+    priceU: number,
 }
 
-export interface ISessionOrSubscription {
+export interface ISelectSubscription {
+    title: string,
+    desc1: string,
+    desc2: string,
+    desc3: string,
+    repro: number,
+    price: string,
+    monthT: number,
+    priceU: number,
+}
+
+export interface ISession {
     adicional: string;
     datetime: string;
     email: string;
     images: string[];
-    monthT?: number;
     name: string;
-    desc?: string;
     priceU: number;
     reference: string;
-    repro?: number;
-    title: string
+    title: string;
+    select?: string;
+    state?: boolean;
+    createdAt?: string;
+    updatedAt?: string;
+    user?: string;
+    valid?: boolean;
+    _id?: string
+}
+
+export interface ISubscription {
+    adicional: string;
+    datetime: string;
+    email: string;
+    images: string[];
+    monthT: number;
+    name: string;
+    desc: string;
+    priceU: number;
+    reference: string;
+    repro: number;
+    title: string;
+    select?: string;
+    state?: boolean;
+    createdAt?: string;
+    updatedAt?: string;
+    user?: string;
+    valid?: boolean;
+    _id?: string
 }
