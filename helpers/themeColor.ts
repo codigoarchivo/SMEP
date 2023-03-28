391bc061c6255d4ff599c5fbb7fb9205c1d46c2e
Jackson Quintero <codigoarchivo@gmail.com>
2023-03-28 18:49:02 -0400

    helpers/themeColor.ts

---
+++ helpers/themeColor.ts
@@ -0,0 +1,7 @@
+import { createTheme } from '@mui/material/styles';
+
+/* Creating a light theme. */
+export const lightTheme = createTheme({ palette: { mode: 'light' } });
+
+/* Creating a dark theme. */
+export const darkTheme = createTheme({ palette: { mode: 'dark' } });
